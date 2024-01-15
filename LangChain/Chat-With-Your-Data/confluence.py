fcc2f58aa6449b94bb6ed195b00c00b7f3e49155
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2024-01-15 11:58:49 +0530

    Create confluence.py

---
+++ LangChain/Chat-With-Your-Data/confluence.py
@@ -0,0 +1,11 @@
+from langchain.document_loaders import ConfluenceLoader
+
+loader = ConfluenceLoader(
+    # URL should be wihtout /wiki/home at the end
+    url="<my url>", 
+    # user name should be with @ your domain at the end
+    username="<my user>", 
+    api_key="<my token>"
+)
+# space id should not be the display name
+documents = loader.load(space_key="<space id>", limit=50)
