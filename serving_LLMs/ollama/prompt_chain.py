65f6d571ee0ea6de25f7d9f03b1bdaaee74e240b
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2025-03-05 19:57:57 +0530

    Create prompt_chain.py

---
+++ serving_LLMs/ollama/prompt_chain.py
@@ -0,0 +1,16 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = OllamaLLM(model="deepseek-r1:7b")
+
+chain = prompt | model
+
+response = chain.invoke({"question": "what is ollama?"})
+
+print (response)
